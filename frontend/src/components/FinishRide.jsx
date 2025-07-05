a7cfe72d00c334932030a28e4ad26ac9b4f87e3e
Dhanvantari <dppawar@gmail.com>
2025-07-05 22:55:51 +0530

    Captain Home UI done

--- frontend/src/components/FinishRide.jsx
+++ frontend/src/components/FinishRide.jsx
@@ -1,16 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
-const RidePopUp = (props) => {
+
+const FinishRide = (props) => {
+
+    const navigate = useNavigate()
+
+    async function endRide() {
+        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/rides/end-ride`, {
+
+            rideId: props.ride._id
+
+
+        }, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+
+        if (response.status === 200) {
+            navigate('/captain-home')
+        }
+
+    }
+
     return (
         <div>
             <h5 className='p-1 text-center w-[93%] absolute top-0' onClick={() => {
-                props.setRidePopupPanel(false)
+                props.setFinishRidePanel(false)
             }}><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
-            <h3 className='text-2xl font-semibold mb-5'>New Ride Available!</h3>
-            <div className='flex items-center justify-between p-3 border-2 border-gray-300 rounded-lg mt-4'>
+            <h3 className='text-2xl font-semibold mb-5'>Finish this Ride</h3>
+            <div className='flex items-center justify-between p-3 border-2 border-yellow-400 rounded-lg mt-4'>
                 <div className='flex items-center gap-3 '>
                     <img className='h-12 rounded-full object-cover w-12' src="https://i.pinimg.com/236x/af/26/28/af26280b0ca305be47df0b799ed1b12b.jpg" alt="" />
-                    <h2 className='text-base font-medium'>{props.ride?.user.fullname.firstname + " " + props.ride?.user.fullname.lastname}</h2>
+                    <h2 className='text-base font-medium'>{props.ride?.user.fullname.firstname}</h2>
                 </div>
                 <h5 className='text-base font-semibold'>2.2 KM</h5>
             </div>
@@ -38,17 +63,12 @@ const RidePopUp = (props) => {
                         </div>
                     </div>
                 </div>
-                <div className='mt-5 w-full '>
-                    <button onClick={() => {
-                        props.setConfirmRidePopupPanel(true)
-                        props.confirmRide()
 
-                    }} className=' bg-green-600 w-full text-white font-semibold p-2 px-10 rounded-lg'>Accept</button>
+                <div className='mt-10 w-full'>
 
-                    <button onClick={() => {
-                        props.setRidePopupPanel(false)
-
-                    }} className='mt-2 w-full bg-gray-300 text-gray-700 font-semibold p-2 px-10 rounded-lg'>Ignore</button>
+                    <button
+                        onClick={endRide}
+                        className='w-full mt-5 flex  text-lg justify-center bg-green-600 text-white font-semibold p-3 rounded-lg'>Finish Ride</button>
 
 
                 </div>
@@ -57,4 +77,4 @@ const RidePopUp = (props) => {
     )
 }
 
-export default RidePopUp
+export default FinishRide
--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
-import CaptainLogout from './CaptainLogout'
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
-import RidePopUp from '../components/RidePopUp'
 import CaptainDetails from '../components/CaptainDetails'
-import { useState, useRef } from 'react'
+import RidePopUp from '../components/RidePopUp'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import 'remixicon/fonts/remixicon.css'
-
 import ConfirmRidePopUp from '../components/ConfirmRidePopUp'
-// import LiveTracking from '../components/LiveTracking'
+import { useEffect, useContext } from 'react'
+//import { SocketContext } from '../context/SocketContext'
+// import { CaptainDataContext } from '../context/CapatainContext'
+import axios from 'axios'
+import CaptainRiding from './CaptainRiding'
+
 const CaptainHome = () => {
 
-   const [ ridePopupPanel, setRidePopupPanel ] = useState(false)
+    const [ ridePopupPanel, setRidePopupPanel ] = useState(true)
     const [ confirmRidePopupPanel, setConfirmRidePopupPanel ] = useState(false)
 
     const ridePopupPanelRef = useRef(null)
     const confirmRidePopupPanelRef = useRef(null)
     const [ ride, setRide ] = useState(null)
+
+    // const { captain } = useContext(CaptainDataContext)
+
+
+    async function confirmRide() {
+
+        // const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/rides/confirm`, {
+
+        //     rideId: ride._id,
+        //     captainId: captain._id,
+
+
+        // }, {
+        //     headers: {
+        //         Authorization: `Bearer ${localStorage.getItem('token')}`
+        //     }
+        // })
+
+        setRidePopupPanel(false)
+        setConfirmRidePopupPanel(true)
+
+    }
 
   useGSAP(function () {
         if (ridePopupPanel) {
@@ -44,38 +67,39 @@ const CaptainHome = () => {
         }
     }, [ confirmRidePopupPanel ])
 
-  return (
-     <div className='h-screen '>
-           <div className='fixed p-2 top-0 flex items-center justify-between w-screen'>
+ return (
+        <div className='h-screen'>
+            <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
                 <img className='w-16' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
                 <Link to='/captain-home' className=' h-10 w-10 bg-white flex items-center justify-center rounded-full'>
-                    <i className="text-lg font-bold ri-arrow-right-circle-line"></i>
+                    <i className="text-lg font-medium ri-logout-box-r-line"></i>
                 </Link>
             </div>
-            <div className='h-1/2 '>
-                <img className="h-full w-full object-cover" src="https://i.pinimg.com/474x/ea/8c/5d/ea8c5d8fb7c702b38de6c66d5f37dc9c.jpg" alt="" />
-                {/* <LiveTracking /> */}
+            <div className='h-3/5'>
+                <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+
             </div>
-             <div className='h-2/5 p-6'>
+            
+            <div className='h-2/5 p-6'>
                 <CaptainDetails />
             </div>
             <div ref={ridePopupPanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
-                <RidePopUp 
+                <RidePopUp
                     ride={ride}
                     setRidePopupPanel={setRidePopupPanel}
                     setConfirmRidePopupPanel={setConfirmRidePopupPanel}
+                    confirmRide={confirmRide}
                 />
             </div>
             <div ref={confirmRidePopupPanelRef} className='fixed w-full h-screen z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
                 <ConfirmRidePopUp
                     ride={ride}
-                    setConfirmRidePopupPanel={setConfirmRidePopupPanel}
-                    setRidePopupPanel={setRidePopupPanel}
-                />
+                    setConfirmRidePopupPanel={setConfirmRidePopupPanel} setRidePopupPanel={setRidePopupPanel} />
             </div>
 
+            
         </div>
-  )
+    )
 }
 
 export default CaptainHome
